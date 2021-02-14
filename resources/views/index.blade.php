57f83fed7be11200135ae2fddc1133f29a87ed19
Cesar Salazar <rasecsv94@gmail.com>
2021-02-14 16:41:07 -0500

    plugin de live chat

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -576,6 +576,31 @@
     Footer
   ============================-->
   <footer id="footer">
+    <!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v9.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution="setup_tool"
+        page_id="111157637676428"
+  logged_in_greeting="Hola!!! En que podemos ayudarlo"
+  logged_out_greeting="Hola!!! En que podemos ayudarlo">
+      </div>
     <div class="container">
       <div class="copyright">
         &copy;<strong></strong> Derechos Reservados
